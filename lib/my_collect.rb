97ef2f0d22acdeaa1137d61a691672c7632c73a1
Brandon Williams <nis350ztt@gmail.com>
2020-07-16 20:55:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,5 +3,10 @@ def my_collect(array)
   new_array = []
   while i < array.length 
     if block_given?
-      
-
+      new_array << yield(array[i])
+    end 
+    i += 1 
+  end 
+  puts new_array.inspect
+  new_array
+end 
